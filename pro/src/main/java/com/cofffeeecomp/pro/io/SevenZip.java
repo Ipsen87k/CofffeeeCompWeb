a751879ec8d70f58110d54eb2098575da111bdb3
Ipsen87k <gyt3218@gmail.com>
2024-08-23 21:47:12 +0000

    sevenzipはまだ

--- pro/src/main/java/com/cofffeeecomp/pro/io/SevenZip.java
+++ pro/src/main/java/com/cofffeeecomp/pro/io/SevenZip.java
@@ -1,13 +1,18 @@
 package com.cofffeeecomp.pro.io;
 
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.util.List;
 
+import org.apache.commons.compress.archivers.sevenz.SevenZOutputFile;
 import org.springframework.web.multipart.MultipartFile;
 
 public class SevenZip implements Compressable,Decompressable{
     private String ext = ".7z";
+    private String outputPath = "";
 
     @Override
     public void decompress(List<MultipartFile> fileLists,String output) {
@@ -17,15 +22,24 @@ public class SevenZip implements Compressable,Decompressable{
 
     @Override
     public void compress(List<MultipartFile> fileLists,Path path) {
-        //outputName = outputName + this.ext;
-        // try(var sevenZip = new SevenZOutputFile(new File(outputName))){
-        //     for (var file:fileLists){
-        //         try(var bis = new BufferedInputStream(file.getInputStream())){
-        //         }
-        //     }
-        // }catch(IOException e){
-        //     e.printStackTrace();
-        // }
+        this.outputPath = path.toString() + this.ext;
+        try(var sevenZip = new SevenZOutputFile(new File(this.outputPath))){
+            for (var file:fileLists){
+                try(var bis = new BufferedInputStream(file.getInputStream())){
+                    var entry = sevenZip.createArchiveEntry(new File(this.outputPath), "entry");
+                    sevenZip.putArchiveEntry(entry);
+
+                    var buffer = new byte[1024];
+                    var nRead=0;
+                    while ((nRead=bis.read(buffer))!=-1) {
+                        sevenZip.write(buffer,0,nRead);
+                    }
+                    sevenZip.closeArchiveEntry();
+                }
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -35,8 +49,7 @@ public class SevenZip implements Compressable,Decompressable{
 
     @Override
     public String getOutputPath() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getOutputPath'");
+        return this.outputPath;
     }
     
 }
